da3c4302d48aad4ae528bf591006bf7e2a4053d3
Navyugyadav <167389754+Navyugyadav@users.noreply.github.com>
2024-04-18 14:48:23 +1000

    Add files via upload

---
+++ MainProgram.java
@@ -0,0 +1,14 @@
+package itech2306.lab6;
+
+public class MainProgram {
+	public static void main(String[] args)
+	{
+		System.out.println("The following people have been here to edit this file: ");
+		System.out.println("\nHello I am Tharindu,How are you guys ?\n");
+		System.out.println("\nHello I am Nafisa");
+		System.out.println("\nHello I am Nikesh");
+		System.out.println("That is all!");
+		System.out.println("This is shubhdeep, it is very nice to work with all");
+		System.out.println("Navyug this side, please help me");
+	}
+}
